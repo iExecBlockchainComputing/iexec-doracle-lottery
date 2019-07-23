4819814d8962c0d5e69909241a95a595d8e51e78
Hadrien Croubois <hc@iex.ec>
2019-07-23 15:47:49 +0200

    new lottery (ERC721)

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,11 +1,12 @@
 pragma solidity ^0.5.10;
 pragma experimental ABIEncoderV2;
 
-import "iexec-solidity/contracts/ERC20_Token/ERC20.sol";
 import "iexec-doracle-base/contracts/IexecDoracle.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 
-contract Lottery is ERC20, IexecDoracle
+contract Lottery is IexecDoracle, ERC721Full
 {
 	using SafeMath for uint256;
 
@@ -13,36 +14,48 @@ contract Lottery is ERC20, IexecDoracle
 	uint256 public constant TRUST  = 0; // No replication
 	string  public constant PARAMS = "";
 
-	/**
-	* token contract for transfers.
-	*/
 	IERC20 public token;
 
-	struct Details
+	enum LotteryStatusEnum
 	{
-		bytes32   oracleCall;
-		uint256   ticketPrice;
-		uint256   potValue;
-		uint256   registrationDeadline;
-		uint256   maxParticipants;
-		address[] participants;
+		NULL,
+		OPEN,
+		ROLLING,
+		FINISHED
+	}
+	struct LotteryMetadata
+	{
+		LotteryStatusEnum status;
+		bytes32 oracleCall;
+		uint256 ticketPrice;
+		uint256 ticketCount;
+		uint256 ticketMaxCount;
+		uint256 potValue;
+		uint256 registrationDeadline;
 	}
 
-	mapping(bytes32 => uint256) public oracleCallToLottery;
-	Details[]                   public lotteryDetails;
+	struct TicketMetadata
+	{
+		uint256 lotteryID;
+		uint256 ticketNumber;
+	}
 
-	event NewLottery    (uint256 indexed id);
-	event NewParticipant(uint256 indexed id, address participant);
-	event NewRoll       (uint256 indexed id, bytes32 taskid);
-	event Reward        (uint256 indexed id, address winner, uint256 value);
-	event Claim         (uint256 indexed id);
+	LotteryMetadata[]                  public lotteryMetadata;
+	mapping(uint256 => TicketMetadata) public ticketMetadata;
+	mapping(bytes32 => uint256       ) public oracleCallToLottery;
+
+	event NewLottery    (uint256 indexed lotteryid);
+	event NewParticipant(uint256 indexed lotteryid, uint256 ticketid);
+	event NewRoll       (uint256 indexed lotteryid, bytes32 taskid);
+	event Reward        (uint256 indexed lotteryid, address winner, uint256 value);
+	event Claim         (uint256 indexed lotteryid);
 
 	constructor(
 		address _iexecHubAddr,
 		address _app,
 		address _dataset,
 		address _workerpool)
-	public IexecDoracle(_iexecHubAddr)
+	public IexecDoracle(_iexecHubAddr) ERC721Full("Lottery Tickets", "LT")
 	{
 		_iexecDoracleUpdateSettings(
 			_app,
@@ -59,166 +72,178 @@ contract Lottery is ERC20, IexecDoracle
 	 ***************************************************************************/
 	function createLottery(
 		uint256 _ticketPrice,
-		uint256 _maxParticipants,
+		uint256 _ticketMaxCount,
 		uint256 _duration)
 	public
 	{
-		uint256 id = lotteryDetails.length++;
+		uint256 lotteryID = lotteryMetadata.length++;
 
-		Details storage details = lotteryDetails[id];
+		LotteryMetadata storage details = lotteryMetadata[lotteryID];
+		details.status               = LotteryStatusEnum.OPEN;
 		details.ticketPrice          = _ticketPrice;
 		details.registrationDeadline = now + _duration;
-		details.maxParticipants      = _maxParticipants;
+		details.ticketMaxCount       = _ticketMaxCount;
 
-		emit NewLottery(id);
+		emit NewLottery(lotteryID);
 	}
 
-	function buyTicket(uint256 _id)
-	public
+	function receiveApproval(address _sender, uint256, address, bytes calldata _ref)
+		external
 	{
-		Details storage details = lotteryDetails[_id];
+		require(msg.sender == address(token));
+		(uint256 lotteryID) = abi.decode(_ref, (uint256));
+		_buyTicket(_sender, lotteryID);
+	}
 
+	function buyTicket(uint256 lotteryID)
+		external
+	{
+		_buyTicket(msg.sender, lotteryID);
+	}
+
+	function _buyTicket(address _buyer, uint256 _lotteryID)
+	internal
+	{
+		// Get lottery details
+		LotteryMetadata storage details = lotteryMetadata[_lotteryID];
+		require(details.status == LotteryStatusEnum.OPEN);
+
+		// Checks
 		require(now <= details.registrationDeadline);
-		require(details.participants.length < details.maxParticipants);
+		require(details.ticketCount < details.ticketMaxCount);
 
-		_transfer(msg.sender, address(this), details.ticketPrice);
-		details.participants.push(msg.sender);
-		details.potValue = details.potValue.add(details.ticketPrice);
+		// Pay in tokens
+		require(token.transferFrom(_buyer, address(this), details.ticketPrice));
 
-		emit NewParticipant(_id, msg.sender);
+		// Emit ticket
+		uint256 ticketID = uint256(keccak256(abi.encode(_lotteryID, details.ticketCount)));
+		_mint(_buyer, ticketID);
+
+		// Ticket metadata - use _setTokenURI instead ?
+		ticketMetadata[ticketID].lotteryID    = _lotteryID;
+		ticketMetadata[ticketID].ticketNumber = details.ticketCount;
+
+		// Details update
+		details.potValue    = details.potValue.add(details.ticketPrice);
+		details.ticketCount = details.ticketCount.add(1);
+
+		emit NewParticipant(_lotteryID, ticketID);
 	}
 
 	function roll(
-		uint256                                  _id,
+		uint256                                  _lotteryID,
 		IexecODBLibOrders.AppOrder        memory _apporder,
 		IexecODBLibOrders.DatasetOrder    memory _datasetorder,
 		IexecODBLibOrders.WorkerpoolOrder memory _workerpoolorder)
 	public
 	{
-		Details storage details = lotteryDetails[_id];
+		// Get lottery details
+		LotteryMetadata storage details = lotteryMetadata[_lotteryID];
+		require(details.status == LotteryStatusEnum.OPEN);
 
+		// Check lottery status
 		require(now > details.registrationDeadline);
 		require(details.oracleCall == bytes32(0));
 
-		// check whitelist
+		// Check whitelist
 		require(m_authorizedApp        == address(0) || checkIdentity(m_authorizedApp,        _apporder.app,               iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-app");
 		require(m_authorizedDataset    == address(0) || checkIdentity(m_authorizedDataset,    _datasetorder.dataset,       iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-dataset");
 		require(m_authorizedWorkerpool == address(0) || checkIdentity(m_authorizedWorkerpool, _workerpoolorder.workerpool, iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-workerpool");
 
+		// Create order
 		IexecODBLibOrders.RequestOrder memory requestorder;
-		requestorder.app                = _apporder.app;                         //address
-		requestorder.appmaxprice        = _apporder.appprice;                    //uint256
-		requestorder.dataset            = _datasetorder.dataset;                 //address
-		requestorder.datasetmaxprice    = _datasetorder.datasetprice;            //uint256
-		requestorder.workerpool         = _workerpoolorder.workerpool;           //address
-		requestorder.workerpoolmaxprice = _workerpoolorder.workerpoolprice;      //uint256
-		requestorder.requester          = address(this);                         //address
-		requestorder.volume             = 1;                                     //uint256
-		requestorder.tag                = TAG;                                   //bytes32
-		requestorder.category           = _workerpoolorder.category;             //uint256
-		requestorder.trust              = TRUST;                                 //uint256
-		requestorder.beneficiary        = address(0);                            //address
-		requestorder.callback           = address(this);                         //address
-		requestorder.params             = PARAMS;                                //string
-		requestorder.salt               = keccak256(abi.encodePacked(now, _id)); //bytes32
+		requestorder.app                = _apporder.app;                                //address
+		requestorder.appmaxprice        = _apporder.appprice;                           //uint256
+		requestorder.dataset            = _datasetorder.dataset;                        //address
+		requestorder.datasetmaxprice    = _datasetorder.datasetprice;                   //uint256
+		requestorder.workerpool         = _workerpoolorder.workerpool;                  //address
+		requestorder.workerpoolmaxprice = _workerpoolorder.workerpoolprice;             //uint256
+		requestorder.requester          = address(this);                                //address
+		requestorder.volume             = 1;                                            //uint256
+		requestorder.tag                = TAG;                                          //bytes32
+		requestorder.category           = _workerpoolorder.category;                    //uint256
+		requestorder.trust              = TRUST;                                        //uint256
+		requestorder.beneficiary        = address(0);                                   //address
+		requestorder.callback           = address(this);                                //address
+		requestorder.params             = PARAMS;                                       //string
+		requestorder.salt               = keccak256(abi.encodePacked(now, _lotteryID)); //bytes32
 
-		// sign order
+		// Sign order
 		require(iexecClerk.signRequestOrder(requestorder));
 
-		// pay for deal
+		// Deposit for deal
 		uint256 dealprice = _apporder.appprice.add(_datasetorder.datasetprice).add(_workerpoolorder.workerpoolprice);
 		token.approve(address(iexecClerk), dealprice);
 		iexecClerk.deposit(dealprice);
-		_burn(address(this), dealprice);
 		details.potValue = details.potValue.sub(dealprice);
 
-		// match and retreive deal
+		// Match and retreive details
 		bytes32 dealid = iexecClerk.matchOrders(_apporder, _datasetorder, _workerpoolorder, requestorder);
 		bytes32 taskid = keccak256(abi.encodePacked(dealid, uint256(0)));
 
-		// register
-		details.oracleCall = taskid;
-		oracleCallToLottery[taskid] = _id;
+		// Register
+		details.status              = LotteryStatusEnum.ROLLING;
+		details.oracleCall          = taskid;
+		oracleCallToLottery[taskid] = _lotteryID;
 
-		emit NewRoll(_id, taskid);
+		emit NewRoll(_lotteryID, taskid);
 	}
 
 	function receiveResult(bytes32 _doracleCallId, bytes memory)
 	public
 	{
-		// emit ResultReady(_doracleCallId);
+		uint256 lotteryID = oracleCallToLottery[_doracleCallId];
 
-		uint256 id              = oracleCallToLottery[_doracleCallId];
-		Details storage details = lotteryDetails[id];
+		// Get lottery details
+		LotteryMetadata storage details = lotteryMetadata[lotteryID];
+		require(details.status == LotteryStatusEnum.ROLLING);
 
-		(uint256 value) = abi.decode(_iexecDoracleGetVerifiedResult(_doracleCallId), (uint256));
+		// Get result
+		(bool success, bytes memory results) = _iexecDoracleGetResults(_doracleCallId);
 
-		address winner = details.participants[value % details.participants.length];
-		_transfer(address(this), winner, details.potValue);
+		if (success)
+		{
+			(uint256 value) = abi.decode(results, (uint256));
 
-		emit Reward(id, winner, details.potValue);
+			// Identify winner
+			uint256 ticketID = uint256(keccak256(abi.encode(lotteryID, value.mod(details.ticketCount))));
+			address winner  = ownerOf(ticketID);
 
-		delete oracleCallToLottery[_doracleCallId];
-		delete lotteryDetails[id];
+			// Reward
+			token.transfer(winner, details.potValue);
+			emit Reward(lotteryID, winner, details.potValue);
+
+			// Lock for good
+			details.status = LotteryStatusEnum.FINISHED;
+		}
+		else if (keccak256(results) != keccak256("result-not-available")) // any error (other than task not finished)
+		{
+			details.status     = LotteryStatusEnum.OPEN;
+			details.oracleCall = bytes32(0);
+		}
 	}
 
-	function claim(uint256 _id)
+	function claim(uint256 _lotteryID)
 	public
 	{
-		Details storage details = lotteryDetails[_id];
+		// Get lottery details
+		LotteryMetadata storage details = lotteryMetadata[_lotteryID];
+		require(details.status == LotteryStatusEnum.ROLLING);
 
+		// Get task & deal
 		IexecODBLibCore.Task memory task = iexecHub.viewTask(details.oracleCall);
 		IexecODBLibCore.Deal memory deal = iexecClerk.viewDeal(task.dealid);
 
+		// Check task status
 		require(task.status == IexecODBLibCore.TaskStatusEnum.FAILLED);
 		uint256 dealprice = deal.app.price.add(deal.dataset.price).add(deal.workerpool.price);
 
+		// Refund
 		iexecClerk.withdraw(dealprice);
-		_mint(address(this), dealprice);
+		details.status     = LotteryStatusEnum.OPEN;
 		details.potValue   = details.potValue.add(dealprice);
 		details.oracleCall = bytes32(0);
 
-		emit Claim(_id);
+		emit Claim(_lotteryID);
 	}
-
-	/***************************************************************************
-	 *                                 ESCROW                                  *
-	 ***************************************************************************/
-	function deposit(uint256 _amount)
-		external returns (bool)
-	{
-		_deposit(msg.sender, _amount);
-		_mint(msg.sender, _amount);
-		return true;
-	}
-
-	function withdraw(uint256 _amount)
-		external returns (bool)
-	{
-		_burn(msg.sender, _amount);
-		_withdraw(msg.sender, _amount);
-		return true;
-	}
-
-	function receiveApproval(address _sender, uint256 _amount, address _token, bytes calldata)
-		external returns (bool)
-	{
-		require(_token == address(token), 'wrong-token');
-		_deposit(_sender, _amount);
-		_mint(_sender, _amount);
-		return true;
-	}
-
-	function _deposit(address _from, uint256 _amount)
-		internal
-	{
-		require(token.transferFrom(_from, address(this), _amount));
-	}
-
-	function _withdraw(address _to, uint256 _amount)
-		internal
-	{
-		token.transfer(_to, _amount);
-	}
-
 }
--- scripts/main.js
+++ scripts/main.js
@@ -6,45 +6,206 @@ const CLERK   = require("iexec-poco/build/contracts-min/IexecClerk.json");
 const LOTTERY = require("../build/contracts/Lottery.json");
 
 const CHAIN           = "kovan";
-const LOTTERY_ADDR    = "0x60e3076D1b513658A0A1fBcb79417A9DB6844fa1";
+const LOTTERY_ADDR    = "0x5eBD616389e5BC21fbbE570a435F081991b9f9F3";
+
 const APP_ADDR        = "0xB43c71cb72A1EA1CAcF3F30F476155F48285F790";
 const DATASET_ADDR    = undefined;
-const WORKERPOOL_ADDR = undefined;
-const CATEGORY        = 2;
+const WORKERPOOL_ADDR = "0xE4F0A428c71e9C9647907Fb339991748fC345413";
+// const WORKERPOOL_ADDR = undefined;
+const CATEGORIES      = [0,1,2,3,4];
+
+ethers.errors.setLogLevel("error");
 
 var lottery = null;
 var rlc     = null;
 
-
-async function deposit(wallet, amount)
+function statusToString(status)
 {
-	return (await rlc.connect(wallet).approveAndCall(lottery.address, amount, "0x")).wait();
-}
-async function viewLottery(id)
-{
-	const details   = await lottery.lotteryDetails(id);
-	const now       = Date.now()/1000;
-	const remaining = details.registrationDeadline - now;
-	const reached   = remaining < 0;
-
-	console.log(`Ticket price:      ${details.ticketPrice.toString()}`);
-	console.log(`Value in pot:      ${details.potValue.toString()}`);
-	console.log(`Number of tickets: ${details.maxParticipants.toString()}`);
-	console.log(`Deadline:          ${details.registrationDeadline.toString()}`);
-	console.log(`Status:            ${reached?"Closed":"Open"}`);
-	if (!reached)
+	switch(status.toString())
 	{
-		console.log(`→ remaining:       ${remaining}`);
+		case '0': return 'NULL';     break;
+		case '1': return 'OPEN';     break;
+		case '2': return 'ROLLING';  break;
+		case '3': return 'FINISHED'; break;
 	}
-	else
-	{
-		console.log(`→ oracleCall:      ${details.oracleCall.toString()}`);
-	};
 }
 
+function viewLottery(lotteryID)
+{
+	return new Promise((resolve, reject) => {
+		lottery
+		.lotteryMetadata(lotteryID)
+		.then(details => {
+			const now       = Date.now()/1000;
+			const remaining = details.registrationDeadline - now;
+			const reached   = remaining < 0;
 
+			console.log(`Status:          ${statusToString(details.status)}`);
+			console.log(`Ticket price:    ${details.ticketPrice.toString()}`);
+			console.log(`Value in pot:    ${details.potValue.toString()}`);
+			console.log(`Tickets sold:    ${details.ticketCount.toString()}`);
+			console.log(`Tickets maximum: ${details.ticketMaxCount.toString()}`);
+			console.log(`Deadline:        ${details.registrationDeadline.toString()}`);
+			console.log(`Crowd sale:      ${reached?"Closed":"Open"}`);
+			if (!reached)
+			{
+				console.log(`→ remaining:       ${remaining}`);
+			}
+			console.log(`→ oracleCall:      ${details.oracleCall.toString()}`);
+		})
+		.catch(() => {
+			console.log(`Invalid lotteryID: ${lotteryID}`);
+		})
+		.finally(() => {
+			resolve(null);
+		});
+	});
+}
 
+function buyTicket(wallet, lotteryID)
+{
+	return new Promise((resolve, reject) => {
+		lottery
+		.lotteryMetadata(lotteryID)
+		.then( ({ ticketPrice }) => {
+			rlc
+			.connect(wallet)
+			.approveAndCall(lottery.address, ticketPrice, ethers.utils.defaultAbiCoder.encode(["uint256"], [lotteryID]))
+			.then(txPromise => {
+				txPromise
+				.wait()
+				.then(resolve)
+				.catch(reject);
+			})
+			.catch(reject);
+		})
+		.catch(reject);
+	});
+}
 
+function getAppOrder(chainId, app)
+{
+	return new Promise((resolve, reject) => {
+		iexec.orderbook.fetchAppOrderbook(chainId, app)
+		.then(orderbook => {
+			resolve(
+				orderbook
+				.appOrders
+				.filter(e => e.status == 'open')
+				// .filter(e => e.order.tag == )
+				.sort((e1, e2) => e1.order.appprice - e2.order.appprice)
+				[0]
+				.order
+			)
+		})
+		.catch(reject);
+	});
+}
+
+function getDatasetOrder(chainId, dataset)
+{
+	return new Promise((resolve, reject) => {
+		if (dataset)
+		{
+			iexec.orderbook.fetchDatasetOrderbook(chainId, dataset)
+			.then(orderbook => {
+				resolve(
+					orderbook
+					.datasetOrders
+					.filter(e => e.status == 'open')
+					// .filter(e => e.order.tag == )
+					.sort((e1, e2) => e1.order.datasetprice - e2.order.datasetprice)
+					[0]
+					.order
+				)
+			})
+			.catch(reject);
+		}
+		else
+		{
+			resolve(iexec.order.NULL_DATASETORDER);
+		}
+	});
+}
+
+function getWorkerpoolOrder(chainId, workerpool = undefined)
+{
+	return new Promise((resolve, reject) => {
+		Promise.all(CATEGORIES.map(cat => iexec.orderbook.fetchWorkerpoolOrderbook(chainId, cat.toString(), {})))
+		.then(orderbooks => {
+			resolve(
+				[]
+				.concat(...orderbooks.map(e => e.workerpoolOrders))
+				.filter(e => e.status == 'open')
+				.filter(e => !workerpool || e.order.workerpool == workerpool)
+				// .filter(e => e.order.tag == )
+				.sort((e1, e2) => e1.order.workerpoolprice - e2.order.workerpoolprice)
+				[0]
+				.order
+			);
+		})
+		.catch(reject);
+	});
+}
+
+function roll(lotteryID)
+{
+	return new Promise(async (resolve, reject) => {
+		const { chainId } = await lottery.provider.ready;
+		Promise.all([
+			getAppOrder       (chainId, APP_ADDR       ),
+			getDatasetOrder   (chainId, DATASET_ADDR   ),
+			getWorkerpoolOrder(chainId, WORKERPOOL_ADDR),
+		])
+		.then(orders => {
+			lottery
+			.roll(lotteryID, ...orders)
+			.then(txPromise => {
+				txPromise
+				.wait()
+				.then(resolve)
+				.catch(reject);
+			})
+			.catch(reject);
+		})
+		.catch(reject);
+	});
+}
+
+function claim(lotteryID)
+{
+	return new Promise(async (resolve, reject) => {
+		lottery
+		.claim(lotteryID)
+		.then(txPromise => {
+			txPromise
+			.wait()
+			.then(resolve)
+			.catch(reject);
+		})
+		.catch(reject);
+	});
+}
+
+function receiveResult(lotteryID)
+{
+	return new Promise((resolve, reject) => {
+		lottery
+		.lotteryMetadata(lotteryID)
+		.then( ({ oracleCall }) => {
+			lottery
+			.receiveResult(oracleCall, "0x")
+			.then(txPromise => {
+				txPromise
+				.wait()
+				.then(resolve)
+				.catch(e => reject("#1: " + e));
+			})
+			.catch(e => reject("#2: " + e));
+		})
+		.catch(reject);
+	});
+}
 
 
 
@@ -53,73 +214,40 @@ async function viewLottery(id)
 
 	const provider = ethers.getDefaultProvider(CHAIN);
 	const wallet   = new ethers.Wallet(process.env.MNEMONIC, provider);
-	const { name, chainId } = await provider.ready;
+	const wallet2  = new ethers.Wallet("0x564a9db84969c8159f7aa3d5393c5ecd014fce6a375842a45b12af6677b12407", provider);
 
-	lottery = new ethers.Contract(LOTTERY_ADDR,               LOTTERY.abi, wallet);
-	clerk   = new ethers.Contract(await lottery.iexecClerk(), CLERK.abi,   wallet);
-	rlc     = new ethers.Contract(await lottery.token(),      RLC.abi,    wallet);
+	lottery = new ethers.Contract(LOTTERY_ADDR,          LOTTERY.abi, wallet);
+	rlc     = new ethers.Contract(await lottery.token(), RLC.abi,     wallet);
 
-	console.log(wallet.address);
 	console.log("------------ BEFORE ------------");
-	console.log("RLC     balance", Number(await rlc.balanceOf(wallet.address)));
-	console.log("Lottery balance", Number(await lottery.balanceOf(wallet.address)));
+	console.log(`Wallet address: ${wallet.address}`);
+	console.log(`RLC balance:    ${(await rlc.balanceOf(wallet.address)).toString()}`);
 	console.log("--------------------------------");
 
-	const lotteryID = 0;
+	const lotteryID = 3;
 
-	if (false)
-	{
-		console.log("DEPOSIT");
-		await deposit(wallet, 360);
-	}
+	// CREATE - OK
+	// await (await lottery.createLottery(10, 10, 600)).wait();
 
-	if (false)
-	{
-		console.log("CREATE & BUY");
+	// BUY - OK
+	// await buyTicket(wallet, lotteryID);
+	// await buyTicket(wallet, lotteryID);
 
-		await (await lottery.createLottery(60, 5, 30)).wait();
-		await (await lottery.buyTicket(lotteryID)).wait();
-		await (await lottery.buyTicket(lotteryID)).wait();
-	}
+	// ROOL - DEBUG
+	// await roll(lotteryID)
 
-	if (false)
-	{
-		console.log("ROOL");
+	// CLAIM - DEBUG
+	// await claim(lotteryID);
 
-		const apporder =
-			(await iexec.orderbook.fetchAppOrderbook(chainId, APP_ADDR))
-			.appOrders
-			.filter(e => e.status == 'open')
-			.sort((e1, e2) => e1.order.appprice - e2.order.appprice)
-			[0]
-			.order
+	// Force result
+	// await receiveResult(lotteryID);
 
-		const datasetorder =
-			DATASET_ADDR
-			? (await iexec.orderbook.fetchDatasetOrderbook(chainId, DATASET_ADDR))
-				.datasetOrders
-				.filter(e => e.status == 'open')
-				.sort((e1, e2) => e1.order.datasetprice - e2.order.datasetprice)
-				[0]
-				.order
-			: iexec.order.NULL_DATASETORDER;
-
-		const workerpoolorder =
-			(await iexec.orderbook.fetchWorkerpoolOrderbook(chainId, CATEGORY.toString(), WORKERPOOL_ADDR ? { WORKERPOOL_ADDR } : {}))
-			.workerpoolOrders
-			.filter(e => e.status == 'open')
-			.sort((e1, e2) => e1.order.workerpoolprice - e2.order.workerpoolprice)
-			[0]
-			.order
-
-		await (await lottery.roll(lotteryID, apporder, datasetorder, workerpoolorder)).wait()
-	}
 
 	await viewLottery(lotteryID);
 
+
 	console.log("------------ AFTER ------------");
-	console.log("RLC     balance", Number(await rlc.balanceOf(wallet.address)));
-	console.log("Lottery balance", Number(await lottery.balanceOf(wallet.address)));
+	console.log(`RLC balance:    ${(await rlc.balanceOf(wallet.address)).toString()}`);
 	console.log("-------------------------------");
 
 
