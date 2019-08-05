3e1d3346bc24d7e3e6d56c3e9c34ec5eaf56bbf5
Hadrien Croubois <hc@iex.ec>
2019-08-05 16:02:38 +0200

    RLC display

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const rlcFormat = (value) => {
 	return value ? value < 10**6
-	? `${value.toString()} nRLC`
-	: `${value.div(10**9).toString()} RLC`
+	? `${Number(value)} nRLC`
+	: `${Number(value)/10**9} RLC`
 	: "---";
 };
 
@@ -13,9 +13,9 @@ const durationFormat = (value) => {
 	const   hours = Math.floor(value /  3600000 % 24);
 	const    days = Math.floor(value / 86400000     );
 
-	if      (   days > 0) return `${days}d ${strPadLeft(hours,'0',2)}h ${strPadLeft(minutes,'0',2)}m ${strPadLeft(seconds,'0',2)}s`;
-	else if (  hours > 0) return `${hours}h ${strPadLeft(minutes,'0',2)}m ${strPadLeft(seconds,'0',2)}s`;
-	else if (minutes > 0) return `${minutes}m ${strPadLeft(seconds,'0',2)}s`;
+	if      (   days > 0) return `${days}d ${strPadLeft(hours,'0', 2)}h ${strPadLeft(minutes,'0',2)}m ${strPadLeft(seconds,'0',2)}s`;
+	else if (  hours > 0) return `${hours}h ${strPadLeft(minutes,'0', 2)}m ${strPadLeft(seconds,'0',2)}s`;
+	else if (minutes > 0) return `${minutes}m ${strPadLeft(seconds,'0', 2)}s`;
 	else                  return `${seconds}s`;
 }
 
